cb5aeec21026349eee5e0e1ef135e547b47f48b4
electrospot105 <adithyanvs105@gmail.com>
2026-03-28 23:20:08 +0530

    s

--- routes/colleges.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getColleges, addCollege } = require('../controllers/collegeController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.get('/', authMiddleware, getColleges);
-router.post('/', authMiddleware, addCollege);
-
-module.exports = router;
